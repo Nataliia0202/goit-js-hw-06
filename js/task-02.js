dfa48b025ffccf734a8823f97cfd2892e42101a8
Nataliia Svystula <natashasvistula@gmail.com>
2022-08-29 21:48:54 +0300

    add 1,2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,28 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector("#ingredients");
+
+const elements = ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.className = "item";
+  itemEl.textContent = ingredient;
+  return itemEl;
+});
+
+console.log(elements);
+
+list.append(...elements);
+
+// const list = document.querySelector("#ingredients");
+// const newArrey = [];
+
+// ingredients.forEach(ingredient => {
+//   const itemEl = document.createElement("li");
+//   itemEl.className = "item";
+//   itemEl.textContent = ingredient;
+//   newArrey.push(itemEl);
+// });
+// list.append(...newArrey);
+
+// console.log(newArrey);
