4258e552293de10bde09617eb4a10f1606bc4233
Nataliia Svystula <natashasvistula@gmail.com>
2022-08-30 16:46:39 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,20 @@
+
+const textInput = document.querySelector("#validation-input");
+
+const textInputLength = textInput.getAttribute("data-length");
+
+console.log(textInputLength);
+
+textInput.addEventListener("blur", event => {
+    if (event.currentTarget.value.length == textInputLength) {
+      textInput.classList.add("valid");
+      if (textInput.classList.contains("invalid")) {
+        textInput.classList.remove("invalid");
+      }
+    } else {
+      if (textInput.classList.contains("valid")) {
+        textInput.classList.remove("valid");
+      }
+      textInput.classList.add("invalid");
+    }
+});
