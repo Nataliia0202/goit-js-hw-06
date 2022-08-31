04755a279b4233a84ee3330dae0f9121958e7523
Nataliia Svystula <natashasvistula@gmail.com>
2022-08-31 15:32:52 +0300

    add task 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    const {
+      elements: { email, password },
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+    const user = { email: email.value, Password: password.value };
+
+    console.log(user);
+    event.currentTarget.reset();
+}
