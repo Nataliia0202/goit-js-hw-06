b5e7c7dd6ea9a300ed3cefa08d5fa43e9718565c
Nataliia Svystula <natashasvistula@gmail.com>
2022-09-01 11:21:03 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,10 @@
 const inputChange = document.querySelector("input#font-size-control");
 const textEl = document.querySelector("span#text");
-
+inputChange.value = 16;
 inputChange.addEventListener("input", onInputChange);
 
 
-function onInputChange(event) {
-    textEl.style.fontSize = `${event.currentTarget.value}px`;
+function onInputChange(event) { 
+     textEl.style.fontSize = event.currentTarget.value + "px" ;
 }
 
